1c2ceac743914339da6bc1f31039a3e19682a716
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-09-01 17:01:44 -0500

    Complete

--- javascript/app.js
+++ javascript/app.js
@@ -1,8 +1,8 @@
 // Self envoking function! once the document is ready, bootstrap our application.
-// We do this to make sure that all the HTML is rendered before we do things 
-// like attach event listeners and any dom manipulation.  
-(function(){
-  $(document).ready(function(){
+// We do this to make sure that all the HTML is rendered before we do things
+// like attach event listeners and any dom manipulation.
+(function() {
+  $(document).ready(function() {
     bootstrapSpotifySearch();
   })
 })();
@@ -10,59 +10,105 @@
 /**
   This function bootstraps the spotify request functionality.
 */
-function bootstrapSpotifySearch(){
+
+var artistInfo = {};
+var albumInfo = {};
+
+function bootstrapSpotifySearch() {
 
   var userInput, searchUrl, results;
   var outputArea = $("#q-results");
 
-  $('#spotify-q-button').on("click", function(){
-      var spotifyQueryRequest;
-      spotifyQueryString = $('#spotify-q').val();
-      searchUrl = "https://api.spotify.com/v1/search?type=artist&q=" + spotifyQueryString;
+  $('#spotify-q-button').on("click", function() {
+    var spotifyQueryRequest;
+    spotifyQueryString = $('#spotify-q').val();
+    searchUrl = "https://api.spotify.com/v1/search?type=artist&q=" +
+      spotifyQueryString;
 
-      // Generate the request object
-      spotifyQueryRequest = $.ajax({
-          type: "GET",
-          dataType: 'json',
-          url: searchUrl
+    // Generate the request object
+    spotifyQueryRequest = $.ajax({
+      type: "GET",
+      dataType: 'json',
+      url: searchUrl
+    });
+
+    // Attach the callback for success
+    // (We could have used the success callback directly)
+    spotifyQueryRequest.done(function(data) {
+      var artists = data.artists;
+
+      // Clear the output area
+      outputArea.html('');
+      var title = "Artist: ";
+      artists.items.forEach(function(artist) {
+        artistInfo = artist;
+        var artistLi = $("<li>" + artist.name + "</li>");
+        artistLi.attr('data-spotify-id', artist.id);
+        outputArea.append(artistLi);
+        artistLi.click(displayAlbums);
       });
+      outputArea.prepend(title);
+    });
 
-      // Attach the callback for success 
-      // (We could have used the success callback directly)
-      spotifyQueryRequest.done(function (data) {
-        var artists = data.artists;
-
-        // Clear the output area
-        outputArea.html('');
-
-        // The spotify API sends back an arrat 'items' 
-        // Which contains the first 20 matching elements.
-        // In our case they are artists.
-        artists.items.forEach(function(artist){
-          var artistLi = $("<li>" + artist.name + " - " + artist.id + "</li>")
-          artistLi.attr('data-spotify-id', artist.id);
-          outputArea.append(artistLi);
-
-          artistLi.click(displayAlbumsAndTracks);
-        })
-      });
-
-      // Attach the callback for failure 
-      // (Again, we could have used the error callback direcetly)
-      spotifyQueryRequest.fail(function (error) {
-        console.log("Something Failed During Spotify Q Request:")
-        console.log(error);
-      });
+    // Attach the callback for failure
+    // (Again, we could have used the error callback direcetly)
+    spotifyQueryRequest.fail(function(error) {
+      console.log("Something Failed During Spotify Q Request:")
+      console.log(error);
+    });
   });
 }
 
-/* COMPLETE THIS FUNCTION! */
-function displayAlbumsAndTracks(event) {
-  var appendToMe = $('#albums-and-tracks');
+/* -------------------- Albums -------------------- */
 
-  // These two lines can be deleted. They're mostly for show. 
-  console.log("you clicked on:");
-  console.log($(event.target).attr('data-spotify-id'));//.attr('data-spotify-id'));
+function displayAlbums(event) {
+  $('#q-results').hide();
+  var appendToMe = $('#albums');
+  var allAlbums = $.ajax({
+    type: "GET",
+    dataType: 'json',
+    url: 'https://api.spotify.com/v1/artists/' + artistInfo.id +
+      '/albums'
+  });
+
+  allAlbums.done(function(data) {
+    var albumName = data.items;
+    var title = "Albums: ";
+    albumName.forEach(function(album) {
+      albumInfo = album;
+      var albumLi = $("<li>" + album.name + "</li>");
+      albumLi.attr('data-spotify-id', album.id);
+      appendToMe.append(albumLi);
+      albumLi.click(displayTracks);
+    });
+    appendToMe.prepend(title);
+  });
+
+}
+
+/* -------------------- Tracks -------------------- */
+
+function displayTracks(event) {
+  $('#albums').hide();
+  var appendToMe = $('#tracks');
+  var allTracks = $.ajax({
+    type: "GET",
+    dataType: 'json',
+    url: 'https://api.spotify.com/v1/albums/' + albumInfo.id +
+      '/tracks'
+  });
+
+  allTracks.done(function(data) {
+    var trackName = data.items;
+    var title = "Tracks: ";
+    trackName.forEach(function(track) {
+      trackInfo = track;
+      var trackLi = $("<li>" + track.name + "</li>");
+      trackLi.attr('data-spotify-id', tracks.id);
+      appendToMe.append(trackLi);
+    });
+    appendToMe.prepend(title);
+  });
 }
 
 /* YOU MAY WANT TO CREATE HELPER FUNCTIONS OF YOUR OWN */
--- readme.md
+++ readme.md
@@ -8,7 +8,7 @@ Outside of the documentation, which is expansive, spotify offers an 'api console
 
 [https://developer.spotify.com/web-api/console/](https://developer.spotify.com/web-api/console/)
 
-If you prefer to use Postman, you can! One of these tools will be useful. The nice thing about the Spotify Web Console is that it has a lot of human readable content to point you in the right direction. 
+If you prefer to use Postman, you can! One of these tools will be useful. The nice thing about the Spotify Web Console is that it has a lot of human readable content to point you in the right direction.
 
 ## Getting the Code to Run
 
@@ -32,22 +32,22 @@ https://api.spotify.com/v1/search?type=artist&q=WHATEVER YOU TYPE
 
 Head over to the API docs for more about the search endpoint! [https://developer.spotify.com/web-api/search-item/](https://developer.spotify.com/web-api/search-item/)
 
-We parse the results, and populate them into the results area. Simple as pie. 
+We parse the results, and populate them into the results area. Simple as pie.
 
 ## Your Task
 
 Your task is to complete the function `displayAlbumsAndTracks`. Right now, when you click one of the results this function is called, but it simply prints to the console. This function should do 3 things:
 
-1. Query the Spotify API for every album produced by the artist you clicked on. 
+1. Query the Spotify API for every album produced by the artist you clicked on.
 2. For each of those albums fetch every track on the album.
 3. Display this information to the user such that:
 	* Albums appear with its release date.
-	* Each album has its tracks displayed before the next album appears. 
+	* Each album has its tracks displayed before the next album appears.
 	* All of this information should be appended to this div: `<div id='albums-and-tracks'>`
 
 This may seem simple, but it won't be. You will be using AJAX heavily; you will be parsing through some serious documentation; you will have to handle race conditions.  
 
-__Bonus__ 
+__Bonus__
 
 Get the tracks 'popularity' metric and show it side by side with the track name.
 
@@ -57,3 +57,66 @@ You'll need to use these two endpoints, but you may also need to use more:
 [https://developer.spotify.com/web-api/console/get-album/](https://developer.spotify.com/web-api/console/get-album/)
 [https://developer.spotify.com/web-api/console/get-artist-albums/](https://developer.spotify.com/web-api/console/get-artist-albums/)
 
+
+Spotify
+
+** User input box will always be a search for an artist name.
+Query the Spotify API for every album produced by the artist you clicked on.
+	find artist number (clicking on artist name currently logs the number to the console), so
+variable with those numbers in an <li> tag already called   artistLi.
+	GET https://api.spotify.com/v1/artists/{id}/albums This will list the albums by that artist
+For each of those albums fetch every track on the album.
+GET https://api.spotify.com/v1/albums/{id}
+^^^use this to list tracks on that album from the returned object.
+	Also use that request to  display release date. For example, the key is "release_date" : "1983",
+An example of the object it returns is here:
+	https://developer.spotify.com/web-api/get-album/
+Display this information to the user such that:
+Albums appear with its release date.
+Each album has its tracks displayed before the next album appears.
+All of this information should be appended to this div: <div id='albums-and-tracks'>
+
+
+Connect to Spotify API by using this link https://api.spotify.com
+Modify the API URL to search tracks and albums
+
+Endpoints will include Get Artist’s Albums and Get an Album’s Tracks
+
+
+
+
+This Gets the albums of whatever artist
+
+
+https://api.spotify.com/v1/albums/{id}/tracks
+This Gets the tracks of albums. Who spilled the damn honey in the fridge????? JOEY seems rather concerned about this….suspicious….
+
+
+
+
+USER STORIES
+
+
+When a user fills in the search bar with an artist name and clicks the button
+  - A list of all the artists that match the search appears
+  - The items in this list are clickable
+When a user clicks on an artist name
+  - A list of the:
+    - artist albums
+    - release dates
+    - tracks
+
+Pseudocode:
+
+When a user fills in the search bar with an artist name and clicks the button:
+ - A list of all the artists that match the search appears
+   - Click event listener fetches the artist information and ID’s with an ajax call
+When a user clicks on an artist name:
+   - Click event listener fetches the album information using artist ID in the querystring
+   - Another ajax call fetches the release date from the album ID
+   - The same call also fetches the list of track names from the album ID
+     - Create div for each album
+     - Fill the new div with release date of album and album name/image
+		 - Create UL for each album
+		 - Then append each UL to the div for that album
+		 - Loop through the array of track names and append each li to the UL
